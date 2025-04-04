c41329f6e090b8d2d55a35775eee444d66e18b74
BravoHugo <hugo.bravo@uabc.edu.mx>
2025-04-03 22:53:17 -0700

    Update p5.asm

--- LIB/p5/p5.asm
+++ LIB/p5/p5.asm
@@ -5,21 +5,56 @@ section .text
 
 _start:
     mov ebx, 0x5C4B2A60
-    mov ebx, matr
+    add ebx, [matr]
     mov eax, ebx
     call pHex_dw
-    mov al, 10
+    call lj
+    xor eax, eax
     mov ax, bx
     push bx
     call pHex_w
-    mov al, 10
+    call lj
     mov al, 8
     mul bl
-    mov [N], ax
+    mov word[N], ax
     call pHex_w
-    mov al, 10
+    call lj
     inc word[N]
+    mov ax, [N]
+    call pHex_w
+    call lj
+    mov ax, bx
+    mov bl, 0xFF
+    div bl
+    call pHex_w
+    call lj
+    mov al, ah
+    mov ah, 0
+    add word[N], ax
+    mov ax, [N]
+    call pHex_w
+    call lj
+    dec word[N]
+    mov ax, [N]
+    call pHex_w
+    call lj
+    lahf
+    call pHex_b
+    call lj
+    pop bx
+    mov ax, bx
+    call pHex_w
+    call lj
+
+    mov eax, 1      ;sys_exit
+    xor ebx, ebx
+    int 0x80
 
 section .data
-    matr dd 0x02208678
+    matr dd 0x2208678
     N dw 0
+
+lj:
+    mov al, 10
+    call putchar
+    ret
