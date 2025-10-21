37abb9428bc689f5a53b1d7e8449977013838a94
BravoHugo <hugo.bravo@uabc.edu.mx>
2025-10-21 18:39:16 +0000

    prac7k

--- pra7/p7.asm
+++ pra7/p7.asm
@@ -20,7 +20,7 @@ _start:
     mov ax, cx
     call pHex_w
     call lj
-    rol cx, 2       ; 0011-1111-0100-1000(0x3F48)   1111-1010-0100-0000(0XFA40)
+    shl cx, 3       ; 0011-1111-0100-1000(0x3F48) --> 1111-1010-0100-0000(0XFA40)
     mov ax, cx
     call pHex_w
     call lj
@@ -32,13 +32,76 @@ _start:
     call pBin_dw
     call lj
     xor esi, 0x40042021 ;(0100-0000-0000-0100-0010-0000-0010-0001)
+    mov eax, esi
     call pBin_dw
+    call lj
+    push esi        ;inciso d
+
+    mov ch, 0xA7        ;inciso e
+    mov al, ch
+    call pHex_b
+    call lj
+    call pBin_b
+    call lj
+    or ch, 0x48     ;(0100-1000)
+    mov al, ch
+    call pBin_b
+    call lj
+
+    mov bp, 0x67DA      ;inciso f
+    mov ax, bp
+    call pHex_w
+    call lj
+    call pBin_w
+    call lj
+    and bp,0xBBAD         ;(1011-1011-1010-1101)
+    mov ax, bp
+    call pBin_w
+    call lj
+
+    mov ax, bp      ;inciso g
+    call pHex_w
+    call lj
+    shr bp, 3       
+    mov ax, bp
+    call pHex_w
+    call lj
+
+    mov eax, ebx        ;inciso h
+    call pHex_dw
+    call lj
+    shr ebx, 5
+    mov eax, ebx
+    call pHex_dw
+    call lj
+
+    mov ax, cx      ;inciso i
+    call pHex_w
+    call lj
+    shl cx, 3
+    mov ax, cx
+    call pHex_w
+    call lj
+
+    pop esi     ;inciso j
+
+    mov eax, esi        ;inciso k
+    mov edx, esi
+    call pHex_dw
+    call lj
+    shl esi, 3
+    shl edx, 1
+    adc esi, edx
+    mov eax, esi
+    call pHex_dw
     call lj
 
     mov eax, 1
     int 0x80
 
 lj:
+    push eax
     mov al, 10
     call putchar
+    pop eax
     ret
