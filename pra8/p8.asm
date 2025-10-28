af1ecf2b8926beedafae06baf24d09a361e6b4e7
BravoHugo <hugo.bravo@uabc.edu.mx>
2025-10-28 17:21:03 +0000

    prac8_1

--- pra8/p8.asm
+++ pra8/p8.asm
@@ -1,1 +1,22 @@
+%include "pc_iox.inc"
 
+section .text
+    global _start
+
+_start:
+call getche         ; 0>=x<=9
+call lj
+sub eax, '9'
+jb rango_arriba
+sub eax, '0'
+jz
+rango_arriba:
+
+
+
+lj:
+    push eax
+    mov al, 10
+    call putchar
+    pop eax
+    ret
